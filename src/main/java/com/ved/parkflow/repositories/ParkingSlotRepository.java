43cf58c6e341841047160e374e5118448e7bc6a3
vedprtpsingh <singhvedprartap14@gmail.com>
2025-08-17 00:29:41 +0530

    WIP: save my local changes before rebase

--- src/main/java/com/ved/parkflow/repositories/ParkingSlotRepository.java
+++ src/main/java/com/ved/parkflow/repositories/ParkingSlotRepository.java
@@ -2,8 +2,8 @@ package com.ved.parkflow.repositories;
 
 import com.ved.parkflow.entities.ParkingSlot;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 public interface ParkingSlotRepository extends JpaRepository<ParkingSlot, Integer> {
     ParkingSlot findByNumberOfSlots(int numberOfSlots);
+    ParkingSlot findBySlotNumber(String slotNumber);
 }
